33209f49d1138e8f027cdfa82eee01734ef6b897
Baraa El-Mallah <baraa.elmallah@gmail.com>
2025-05-26 17:41:52 +0000

    // Import the functions you need from the SDKs you need import { initializeApp } from "firebase/app"; import { getAnalytics } from "firebase/analytics"; // TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries
    // Your web app's Firebase configuration
    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
    const firebaseConfig = {
      apiKey: "AIzaSyCUIRYm2CbeA0TVJndd5GEa_fDlO0QdeFU",
      authDomain: "harmun-tracker.firebaseapp.com",
      projectId: "harmun-tracker",
      storageBucket: "harmun-tracker.firebasestorage.app",
      messagingSenderId: "920897622876",
      appId: "1:920897622876:web:39df153705816e4345e799",
      measurementId: "G-2GC9MRVG8Q"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);
    const analytics = getAnalytics(app);

---
+++ src/lib/firebase.ts
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics, isSupported as isAnalyticsSupported } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUIRYm2CbeA0TVJndd5GEa_fDlO0QdeFU",
+  authDomain: "harmun-tracker.firebaseapp.com",
+  projectId: "harmun-tracker",
+  storageBucket: "harmun-tracker.appspot.com", // Corrected common typo: .appspot.com for storageBucket
+  messagingSenderId: "920897622876",
+  appId: "1:920897622876:web:39df153705816e4345e799",
+  measurementId: "G-2GC9MRVG8Q"
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+let analytics;
+if (typeof window !== 'undefined') {
+  isAnalyticsSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { app, analytics, firebaseConfig };
