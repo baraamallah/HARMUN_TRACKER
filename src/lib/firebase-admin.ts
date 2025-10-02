7dcae61ea164c635f512ba07b3ee53fdc30b57b5
batman300 <baraa.elmallah@gmail.com>
2025-10-02 17:52:33 +0300

    actions update

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,30 @@
-
 import * as admin from 'firebase-admin';
 
-if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      projectId: 'harmun-tracker',
-    });
-  } catch (error) {
-    console.error('Firebase admin initialization error', error);
-  }
+let adminDb: admin.firestore.Firestore;
+
+function getServiceAccount() {
+    const key = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+    if (!key) {
+        throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY environment variable not set.');
+    }
+    // The key is expected to be a base64 encoded string.
+    const decodedKey = Buffer.from(key, 'base64').toString('utf-8');
+    return JSON.parse(decodedKey);
 }
 
-const adminDb = admin.firestore();
-export { adminDb };
+export function getAdminDb() {
+  if (!admin.apps.length) {
+    try {
+      const serviceAccount = getServiceAccount();
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    } catch (error) {
+      console.error('Firebase admin initialization error', error);
+    }
+  }
+  if (!adminDb) {
+    adminDb = admin.firestore();
+  }
+  return adminDb;
+}
