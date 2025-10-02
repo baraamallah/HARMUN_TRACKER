e6b443089e14f724e97a31e7c894345c9e5d49a0
batman300 <baraa.elmallah@gmail.com>
2025-10-02 18:16:47 +0300

    Update firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,22 +1,9 @@
 
 import * as admin from 'firebase-admin';
 
-function getServiceAccount() {
-    const key = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-    if (!key) {
-        throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY environment variable not set.');
-    }
-    // The key is expected to be a base64 encoded string.
-    const decodedKey = Buffer.from(key, 'base64').toString('utf-8');
-    const correctedKey = decodedKey.replace(/\\n/g, '\n');
-    return JSON.parse(correctedKey);
-}
-
 if (!admin.apps.length) {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert(getServiceAccount()),
-    });
+    admin.initializeApp();
   } catch (error) {
     console.error('Firebase admin initialization error', error);
   }
