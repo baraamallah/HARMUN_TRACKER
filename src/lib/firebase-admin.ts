485a5a605e3cee524b44902df47881a38a9a436f
batman300 <baraa.elmallah@gmail.com>
2025-10-02 17:45:07 +0300

    Update firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,8 +1,11 @@
+
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    admin.initializeApp();
+    admin.initializeApp({
+      projectId: 'harmun-tracker',
+    });
   } catch (error) {
     console.error('Firebase admin initialization error', error);
   }
